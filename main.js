3c5dcc134725013628f22911f9b2eda45d586143
Bethanhelen <bethanhelen8@gmail.com>
2022-06-22 13:38:01 +0100

    file
    the first commit of html and js file

---
+++ main.js
@@ -0,0 +1,13 @@
+alert('My name is Elizabeth Olusipe, I am 5ft 6in tall, from Nigeria')
+
+let fullname = 'Elizabeth Olusipe'
+
+let height = '5ft 6in'
+
+let country = 'Nigeria'
+
+console.log('My name is' + ' ' + fullname)
+
+console.log('My height is' + ' ' + height)
+
+console.log('My country is' + ' ' + country)
